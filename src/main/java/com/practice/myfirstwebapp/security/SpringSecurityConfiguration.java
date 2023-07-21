50aefe3f4bf54e752efa10fda045e1921a1ba033
Malisha A.P.D <128911037+Malisha4065@users.noreply.github.com>
2023-07-22 01:11:52 +0530

    can handle multiple users

--- src/main/java/com/practice/myfirstwebapp/security/SpringSecurityConfiguration.java
+++ src/main/java/com/practice/myfirstwebapp/security/SpringSecurityConfiguration.java
@@ -18,12 +18,17 @@ public class SpringSecurityConfiguration {
 
     @Bean
     public InMemoryUserDetailsManager createUserDetailsManager() {
+        UserDetails userDetails1 = createNewUser("malisha", "dummy");
+        UserDetails userDetails2 = createNewUser("dushmin", "smartguy");
+
+        return new InMemoryUserDetailsManager(userDetails1, userDetails2);
+    }
+
+    private UserDetails createNewUser(String username, String password) {
         Function<String, String> passwordEncoder = input -> passwordEncoder().encode(input);
-
         UserDetails userDetails =
-        User.builder().passwordEncoder(passwordEncoder).username("malisha").password("dummy").roles("USER", "ADMIN").build();
-
-        return new InMemoryUserDetailsManager(userDetails);
+        User.builder().passwordEncoder(passwordEncoder).username(username).password(password).roles("USER", "ADMIN").build();
+        return userDetails;
     }
 
     @Bean
